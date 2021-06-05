c0c5fd58291d156182e31fffddde8890ba8eaa8b
Harrison Burt <57491488+ChillFish8@users.noreply.github.com>
2021-06-05 13:16:52 +0100

    update cache to support 0 size

--- src/cache.rs
+++ src/cache.rs
@@ -17,30 +17,37 @@ pub static CACHE_STATE: OnceCell<CacheState> = OnceCell::new();
 /// A wrapper around the `CacheStore` type letting it be put into Gotham's
 /// shared state.
 #[derive(Clone)]
-pub struct CacheState(pub CacheStore);
+pub struct CacheState(pub Option<CacheStore>);
 
 impl CacheState {
     /// Creates a new cache state instance with a given size.
     pub fn init(cache_size: usize) {
-        let store = Arc::new(ARCache::new_size(cache_size, 12));
-        let inst = Self { 0: store };
+        let inst = if cache_size == 0 {
+            Self { 0: None }
+        } else {
+            let store = Arc::new(ARCache::new_size(cache_size, 12));
+            Self { 0: Some(store) }
+        };
 
         let _ = CACHE_STATE.set(inst);
     }
 
     /// Get a item from the cache if it exists otherwise returns None.
     pub fn get(&self, file_id: Uuid, preset: String, format: ImageFormat) -> Option<BytesMut> {
+        let state = self.0.as_ref()?;
         let ref_val = (file_id, preset, format);
-        let target = self.0.read();
+        let target = state.read();
         target.get(&ref_val).map(|v| v.clone())
     }
 
     /// Adds an item to the cache, if the cache size is already at it's limit
     /// the least recently used (LRU) item is removed.
     pub fn set(&self, file_id: Uuid, preset: String, format: ImageFormat, data: BytesMut) {
-        let ref_val = (file_id, preset, format);
-        let mut target = self.0.write();
-        target.insert(ref_val, data);
-        target.commit();
+        if let Some(state) = self.0.as_ref() {
+            let ref_val = (file_id, preset, format);
+            let mut target = state.write();
+            target.insert(ref_val, data);
+            target.commit();
+        }
     }
 }
